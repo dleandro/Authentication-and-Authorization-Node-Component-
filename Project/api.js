f7ea7513e81fe09cc981514278c9e35808b03f7a
unknown <A44857@alunos.isel.pt>
2020-03-14 18:37:34 +0000

    Started Project

---
+++ Project/api.js
@@ -0,0 +1,11 @@
+"use strict"
+
+module.exports=function(router){
+    router.get('/',homepage)
+
+
+funcion homepage(req,res){
+
+}
+
+}
---
+++ Project/auth.js
@@ -0,0 +1,6 @@
+module.exports={
+   
+    hasPermissions:function hasPermissions(req){
+        return true
+    }
+}

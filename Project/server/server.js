d50a11dd59c94a6e137f8f3e85e2256d1ed46cef
unknown <A44857@alunos.isel.pt>
2020-03-21 22:37:22 +0000

    Passport working

--- Project/server/server.js
+++ Project/server/server.js
@@ -10,6 +10,8 @@ SamlStrategy = require('passport-saml').Strategy,
 path = require("path"),
 data = require('./dal/data'),
 service = require('./service')(data),
+bodyParser = require('body-parser'),
+cookieParser = require('cookie-parser'),
 app = express()
 
 var cookie_secret = 'justasecretstring' // should change after a while if it has some security implication
@@ -46,6 +48,25 @@ passport.use(new SamlStrategy(
 //   serialize users into and deserialize users out of the session.  Typically,
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing. 
+
+// app configurations
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.json()) // Makes it easier to manage the request's body
+// Homepage leads to our web app, make sure web app has updated production build
+app.use('/', express.static(path.resolve(__dirname, '..', 'web-app', 'build'))) 
+app.use(session({
+    resave: false,
+    //saveUninitialized to false to only create a session if a UA(User agent) made a login
+    saveUninitialized: false,
+    secret: cookie_secret
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+// routes and their behaviour
+const api = require("./web-api")(app, service, passport)
+
 function userToRef(user, done) {
   done(null, user.id);
 }
@@ -62,23 +83,6 @@ function refToUser(userRef, done) {
 
 passport.serializeUser(userToRef);
 passport.deserializeUser(refToUser);
-
-// app configurations
-app.use(express.json()) // Makes it easier to manage the request's body
-// Homepage leads to our web app, make sure web app has updated production build
-app.use('/', express.static(path.resolve(__dirname, '..', 'web-app', 'build'))) 
-app.use(session({
-    resave: false,
-    //saveUninitialized to false to only create a session if a UA(User agent) made a login
-    saveUninitialized: false,
-    secret: cookie_secret,
-    cookie: { secure: true }
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
-// routes and their behaviour
-const api = require("./web-api")(app, service, passport)
 
 
 app.listen(DEFAULT_PORT, () => console.log(`Listening on Port:${DEFAULT_PORT} `))

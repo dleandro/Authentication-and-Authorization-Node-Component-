319fa871baae90f507f6272256c683786763ba9b
Tdm9 <48017174+Tdm9@users.noreply.github.com>
2020-04-03 18:38:45 +0100

    react router implemented, sidenavBar implemented, redirect to backoffice on sucessfull login

--- web-app/src/components/LoginForm.js
+++ web-app/src/components/LoginForm.js
@@ -2,24 +2,30 @@ import React from 'react'
 import InputGroup from 'react-bootstrap/InputGroup'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
+const fetch = require('node-fetch');
 
-export default function LoginForm({id , app}) {
 
-    const fetch = require('node-fetch');
+export default function LoginForm({id , app: state,props: setRedirect}) {
+
+    var user, pass ="";
+
+
     function submitLoginRequest() {
-
-        fetch("http://localhost:8082/login"+ app.state.selectedProtocol, {
+        fetch("http://localhost:8082/login"+ state.selectedProtocol, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                username: app.state.username,
-                password: app.state.password
+                username: user,
+                password: pass
             })
         })
-            .then(resp=>resp.json())
-            .then(json=>console.log(json))
+
+          .then(resp=>{setRedirect()})
+           // .then(json=>console.log(json))
+
+        //return <Redirect to="/backofice"/>
     }
 
     function submitGoogleLoginRequest() {
@@ -28,15 +34,12 @@ export default function LoginForm({id , app}) {
     }
 
     const handlePassword = event => {
-        
-        app.setState({ password: event.target.value })
-
-    } 
+        pass= event.target.value
+    }
 
     const handleUsername = event => {
-
-        app.setState({ username: event.target.value })
-    } 
+        user= event.target.value
+    }
 
     return (
         <div className="col-12 form-input" id={id}>
@@ -47,7 +50,6 @@ export default function LoginForm({id , app}) {
                     aria-label="Recipient's username"
                     aria-describedby="basic-addon2"
                     type="text"
-                    value={app.state.username}
                     onChange={handleUsername}
                 />
                 <FormControl
@@ -55,23 +57,19 @@ export default function LoginForm({id , app}) {
                     aria-label="Recipient's password"
                     aria-describedby="basic-addon2"
                     type="password"
-                    value={app.state.password}
                     onChange={handlePassword}
                 />
 
             </InputGroup>
 
 
-            <Button
-                variant="primary"
+            <Button variant="primary"
                 onClick={submitLoginRequest}
             >
-            {'Login'}
+                {'Login'}
             </Button>
 
             <a href="http://localhost:8082/google-login"> Login With Google </a>
-		
         </div>
     )
 }
-

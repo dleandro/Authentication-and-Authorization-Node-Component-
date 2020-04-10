d22ef9cd4be26956a4c007fbcc95f348295bd671
unknown <A44857@alunos.isel.pt>
2020-04-10 04:12:14 +0100

    Database scripts added

---
+++ reset_database.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS Roles_Permission ;
+DROP TABLE IF EXISTS Users_Roles;
+DROP TABLE IF EXISTS Lists;
+DROP TABLE IF EXISTS Users_History;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Roles;
+DROP TABLE IF EXISTS Permission;
+
+CREATE TABLE Roles_Permission(role INT ,permission INT,PRIMARY KEY(role,permission) );
+CREATE TABLE Users_Roles(id INT AUTO_INCREMENT,user_id INT,role_id INT,start_date DATETIME,end_date DATETIME,updater INT,active BIT(1),PRIMARY KEY (id));
+CREATE TABLE Lists(id INT AUTO_INCREMENT,user_id INT ,LIST VARCHAR(20),start_date Datetime,end_date DATETIME ,updater INT,active BIT(1),PRIMARY KEY(id));
+CREATE TABLE Users_History (user_id INT,DATE DATETIME,description VARCHAR(30));
+CREATE TABLE Permission(method VARCHAR(10),path VARCHAR(50));
+CREATE TABLE Roles(id INT AUTO_INCREMENT,role VARCHAR(20) UNIQUE,parent_role INT,PRIMARY KEY(id));
+CREATE TABLE Users(id int AUTO_INCREMENT,username VARCHAR(30) UNIQUE,PASSWORD VARCHAR(20),PRIMARY KEY(id));
--- server/data/dal/role.js
+++ server/data/dal/role.js
@@ -5,18 +5,6 @@ module.exports =  function(dalUtils, errors) {
     return {
         
         addRole: async (role) => {
-        
-            // not gonna work the duplicate function
-            try {
-                // if there already exists users with these given parameters than we have to throw an error
-                // our app doesn't support duplicate users
-                dalUtils.throwErrorIfNecessary(
-                    () => dalUtils.duplicateValues(role),
-                    errors.duplicateValues)
-        
-            } catch(error) {
-                throw error
-            }
             
             try {
                 return await dalUtils.executeQuery(`INSERT INTO Roles(role) VALUES (?);`,
@@ -34,7 +22,7 @@ module.exports =  function(dalUtils, errors) {
         
             try {
         
-                result = await dalUtils.executeQuery('Select * from Users where id= ?', queryParams)
+                result = await dalUtils.executeQuery('Select * from Roles where id= ?', queryParams)
                 
         
             } catch (error) {

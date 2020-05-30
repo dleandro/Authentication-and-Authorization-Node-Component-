edb63335453aa6f66f21afb2552a8915a3c9ed73
Tdm9 <48017174+Tdm9@users.noreply.github.com>
2020-05-30 12:47:24 +0100

    IDP's login buttons and cleanup of unused dependencies

--- web-app/src/components/javascript/routesSrc/loginComponents/LoginForm.js
+++ web-app/src/components/javascript/routesSrc/loginComponents/LoginForm.js
@@ -4,24 +4,28 @@ import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import {authenticationService} from '../../service'
 import UserContext from "../../../Context";
+import GoogleButton from 'react-google-button'
 
 import '../../../stylesheets/App.css'
 const IDP_BASE_URL = "http://localhost:8082/api/authentications";
-const googleStyle = { 		"display":'inline-block',
-        "position":'relative',
-        "margin":'0 auto 0 auto',
-    '-moz-border-radius':'50%',
-'-webkit-border-radius':'50%',
-'border-radius':'50%',
-'text-align':'center',
-'width': '50px',
-height: '50px',
-'font-size':'20px',
-color:'#ffffff',
-'background-color': '#BD3518',
-'line-height':'50px'
-}
 
+const authStyle= {
+    width: '100px',
+    height: '50px',
+}
+const officeStyle= {
+    width: '75px',
+    height: '25px',
+}
+const officeButtonStyle = {border: '1px solid #ea3e23', color: '#ea3e23',
+    'width': '100px', height: '35px',}
+const googleStyle = {border: '1px solid #ea3e23', color: '#ea3e23',
+'width': '150px', height: '70px',}
+
+const centered= {
+    marginLeft: "30px",
+    marginBottom: "10px"
+}
 
 export default function LoginForm({id, app: state, setRedirect}) {
     const userctx = useContext(UserContext)
@@ -53,15 +57,22 @@ export default function LoginForm({id, app: state, setRedirect}) {
                 <FormControl placeholder="password" aria-label="Recipient's password" aria-describedby="basic-addon2"
                              type="password" onChange={handlePassword}/>
             </InputGroup>
-            <a href="#" className="fa fa-google mx-auto" style={googleStyle}></a>
+
             <Button variant="primary" onClick={loginMe}>{'Login'}</Button>
-            <div className="Office-container">
-                <div className="Office-image" />
-                <div className="Office-button-text">office 365</div>
-            </div>
-            <p><a href={`${IDP_BASE_URL}/google`}> Login With Google </a></p>
-            <p><a href={`${IDP_BASE_URL}/saml`}> Login With Oauth0 </a></p>
-            <p><a href={`${IDP_BASE_URL}/azureAD`}> Login With Office 365 </a></p>
+            <GoogleButton style={centered} onClick={() => setRedirect(`${IDP_BASE_URL}/google`)}/>
+
+            <div className="Office-image"  onClick={() => setRedirect(`${IDP_BASE_URL}/azureAD`)} />
+            <div className="Office-button-text" onClick={() => setRedirect(`${IDP_BASE_URL}/azureAD`)}>office 365</div>
+            <Button style={officeButtonStyle} variant="outline-light" onClick={() => setRedirect(`${IDP_BASE_URL}/azureAD`) } >
+                <img  style={officeStyle} src="office365Logo.png"/>
+            </Button>
+
+            <Button style={googleStyle} variant="outline-light" onClick={() => setRedirect(`${IDP_BASE_URL}/saml`)} >
+                <img style={authStyle} src="https://www.drupal.org/files/project-images/auth0-logo-whitebg.png" />
+            </Button>
+
+
+
         </div>
     )
 }

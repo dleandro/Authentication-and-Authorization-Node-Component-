240cc4560d94f66d676e896c77cb65b9e8ef2209
A44857 <A44857@alunos.isel.pt>
2020-06-02 02:41:48 +0100

    Module with db arg

--- web-api/common/middleware/app-middleware.js
+++ web-api/common/middleware/app-middleware.js
@@ -4,12 +4,12 @@ const
     morgan = require('morgan')
 
 // This module is used to setup middleware on the app passed as a parameter
-module.exports = function (app) {
+module.exports = function (app,db) {
 
     // app configurations
 
     // using authization module to setup authentication and authorization middleware
-    require('../../../authization-module/authization')(app)
+    require('../../../authization-module/authization')(app,db)
 
     // For request logging
     app.use(morgan('tiny'))
